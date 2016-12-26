13ead54a0cdb4608d65b4acdba2f2a0ff6b038a2
shibboleet <m@DESKTOP-MNSEM02.(none)>
2016-12-26 16:50:53 -0500

    add files

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # binunpack
-A program for unpacking the BIN archives in DragonBall Revenge of King Piccolo
+A program for unpacking the BIN archives in DragonBall Revenge of King Piccolo, written in python 3.
+
+# How To Use
+The syntax of the program is simple:
+
+```
+binextract.py <infile>
+```
+
+then the program will export the files (all will be binary files, there is no pointer table for filenames, sadly) to a folder, relative where the python script is placed.
---
+++ binextract.py
@@ -0,0 +1,68 @@
+import struct, sys, os
+
+offsets = []
+isLast = False
+
+def parseBIN(count):
+    # offset is always 0x10 so no need for calculations
+    i = 0
+    j = 0
+    offset = 16 # 0x10
+    while j < count:
+        fileOffset, = struct.unpack_from('>I', data, offset + i)
+        offsets.append(fileOffset)
+        i+=4
+        j+=1
+
+def dumpData(src, offset, index):
+    # this is to make sure we don't read OOB
+    # attempt to read an index further
+    try:
+        index+=1
+        nextOffset = offsets[index]
+        isLast = False
+    except:
+        nextOffset = None
+        isLast = True
+
+    if (isLast):
+        end = len(src)
+        bytesToRead = end - offset
+    else:
+        end = nextOffset
+        bytesToRead = end - offset
+
+    dataToWrite = struct.unpack_from('>%dB' % bytesToRead, data, offset)
+
+    # now we determine the name of the folder we put the data in
+    folderName = os.path.basename(sys.argv[1]).rstrip('.bin')
+
+    # and we create it
+    if not os.path.exists(folderName):
+        os.makedirs(folderName)
+        
+    toWrite = data[offset : offset + bytesToRead]
+    
+    with open(folderName + '/%04d.bin' % index, 'wb') as f:
+        print('Writing file %04d.bin...' % index)
+        f.write(toWrite)
+
+    print('Done!')
+
+if len(sys.argv) == 2:
+    with open(sys.argv[1], 'rb') as f:
+        data = f.read()
+else:
+    print('Bin Extractor v0.1 by shibboleet')
+    print('Syntax: binextractor.py <infile>')
+    sys.exit()
+
+# the rest is padding, we only need to read the first 4 bytes
+count, = struct.unpack_from('>I', data, 0)
+
+parseBIN(count)
+
+k = 0
+for offset in offsets:
+    dumpData(data, offset, k)
+    k+=1
